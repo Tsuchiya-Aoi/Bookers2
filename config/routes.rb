0a1b982fa8bdc2d3a457130d99b26559128ae89b
Tsuchiya-Aoi <aoi0u0t@gmail.com>
2022-09-20 11:18:58 +0000

    [Update]ユーザーページに投稿機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'homes/about' => 'homes#about', as: 'about'
-
-  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
   resources :users, only: [:index, :show, :edit, :update]
+  resources :books, only: [:new, :create, :index, :show, :edit, :update, :destroy]
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
