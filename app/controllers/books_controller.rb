6365b7e91a9af995d1d5b0bfb12f17964de7fb3d
Tsuchiya-Aoi <aoi0u0t@gmail.com>
2022-09-20 06:35:14 +0000

    [Update]usersコントローラの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,7 +7,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book)
   end
 
   def index
@@ -25,7 +25,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
-    redirect_to book_path
+    redirect_to book_path(@book)
   end
 
   def destroy
